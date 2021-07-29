6bb21c1dc58cc108044e076276c357357531f78b
Dev-Jair <86578182+Dev-Jair@users.noreply.github.com>
2021-07-29 20:39:20 +0200

    Add files via upload

---
+++ Opdracht_eerste_JavaScript/script.js
@@ -0,0 +1,17 @@
+console.log('Hello Winc Academy');
+let myName = 'Jairzinho';
+console.log(myName);
+
+// Vraag 6: Je kan geen variabel met de naam if aanmaken, omdat het gebruik hiervan wordt gezien als een if- of if-else statement.
+
+4 + 4
+console.log(8);
+"4" + "4"
+console.log("8");
+16 - 3
+console.log(13);
+27 / 9
+console.log(3)
+
+let myAge = 9
+console.log(typeof 9);
